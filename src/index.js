900dc220f95478865bce3663f9b2dca6342d3de7
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2022-06-13 15:16:53 -0300

    projeto requerido js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let nome            = "Leonardo"
+let sobreNome       = "Neves"
+let idade           = 21
+let cidade          = "Rio de Janeiro"
+let anoNascimento   = 2021 - 21
+console.log(`Eu sou o ${nome} ${sobreNome} tenho ${idade} anos, moro no ${cidade} e nasci em ${anoNascimento}`)
